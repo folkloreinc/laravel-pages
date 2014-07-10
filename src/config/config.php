e01a93a6c61181d036083028423ba977fe3014dd
David Mongeau-Petitpas <dmp@atelierfolklore.ca>
2014-07-10 16:08:01 -0400

    Removed config from the workbench

--- src/config/config.php
+++ src/config/config.php
@@ -35,35 +35,9 @@ return array(
 	*/
 	'page_types' => array(
 		'default' => array(
-			'label' => trans('admin.pages_type_default'),
-			'route' => array('pages.show.'.Config::get('app.locale'), function($page) {
+			'label' => 'default',
+			'route' => array('page.'.Config::get('app.locale'), function($page) {
 				return array($page->locale->slug);
-			})
-		),
-		'home' => array(
-			'label' => trans('admin.pages_type_home'),
-			'route' => 'home.'.Config::get('app.locale')
-		),
-		'news' => array(
-			'label' => trans('admin.pages_type_news'),
-			'route' => array('news.show.'.Config::get('app.locale'), function($page) {
-				return array($page->created_at->format('Y'),$page->locale->slug);
-			})
-		),
-		'contact' => array(
-			'label' => trans('admin.pages_type_contact'),
-			'route' => array('pages.show.'.Config::get('app.locale'), function($page) {
-				return array($page->locale->slug);
-			})
-		),
-		'item' => array(
-			'label' => trans('admin.pages_type_item'),
-			'route' => array('store.show.'.Config::get('app.locale'), function($page) {
-				if($page->categories->isEmpty()) {
-					return array('items',$page->locale->slug);
-				} else {
-					return array($page->categories->first()->locale->slug,$page->locale->slug);
-				}
 			})
 		)
 	),
@@ -75,13 +49,7 @@ return array(
 	*/
 	'block_types' => array(
 		'text' => array(
-			'label' => trans('admin.pages_block_type_text')
-		),
-		'photos' => array(
-			'label' => trans('admin.pages_block_type_photos')
-		),
-		'slideshow' => array(
-			'label' => trans('admin.pages_block_type_slideshow')
+			'label' => 'Text'
 		)
 	),
 
@@ -91,14 +59,8 @@ return array(
 	*
 	*/
 	'block_areas' => array(
-		'slideshow' => array(
-			'label' => trans('admin.pages_block_area_slideshow')
-		),
-		'left' => array(
-			'label' => trans('admin.pages_block_area_left')
-		),
-		'right' => array(
-			'label' => trans('admin.pages_block_area_right')
+		'main' => array(
+			'label' => 'Main'
 		)
 	)
 
